d569e11a4a948d0c44343f45fe18abfda3fc0713
kai <aaaa666657>
2021-05-31 06:30:55 +0000

    Don't delete my path please

---
+++ Control/pkg/db/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+const (
+	userName = "root"
+	password = ""
+	host     = "127.0.0.1"
+	port     = "3306"
+	dbName   = "dbName"
+)
+
+func initDB() {
+	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	// "username:password@tcp(host:port)/數據庫?charset=utf8"
+	path := strings.Join([]string{userName, ":", password, "@tcp(", host, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	fmt.Println(path)
+	// 第一個是 driverName 第二個則是 database 的設定 path
+	// 也可以用 var DB *sql.DB
+	DB, _ := sql.Open("mysql", path)
+	// 設定 database 最大連接數
+	DB.SetConnMaxLifetime(100)
+
+	//設定上 database 最大閒置連接數
+	DB.SetMaxIdleConns(10)
+
+	// 驗證是否連上 db
+	if err := DB.Ping(); err != nil {
+		fmt.Println("opon database fail:", err)
+		return
+	}
+	fmt.Println("connnect success")
+}
--- Control/server/main.go
+++ Control/server/main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"log"
 	"net"
-	"strings"
 
 	scannerPB "control/proto/scanner"
 
-	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
-)
-
-const (
-	userName = "root"
-	password = "scanner"
-	host     = "127.0.0.1"
-	port     = "3306"
-	dbName   = "scanner"
 )
 
 type Server struct{}
@@ -41,28 +30,12 @@ func (*Server) Register(ctx context.Context, req *scannerPB.ResourceRegister) (*
 
 func main() {
 	//DB
-	path := strings.Join([]string{userName, ":", password, "@tcp(", host, ":", port, ")/", dbName, "?charset=utf8"}, "")
-	fmt.Println(path)
-
-	DB, _ := sql.Open("mysql", path)
-
-	// 設定 database 最大連接數
-	DB.SetConnMaxLifetime(100)
-
-	//設定上 database 最大閒置連接數
-	DB.SetMaxIdleConns(10)
-
-	// 驗證是否連上 db
-	if err := DB.Ping(); err != nil {
-		fmt.Println("opon database fail:", err)
-		return
-	}
-	fmt.Println("connnect success")
+	db.initDB()
 	//gRPC Server
 	fmt.Println("starting gPRC seerver...")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		//log.Fatal("failed to listen: %v \n", err)
+		log.Fatal("failed to listen: %v \n", err)
 	}
 
 	grpcServer := grpc.NewServer()
