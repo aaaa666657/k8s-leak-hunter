a398c6cb265d4f062e00107957730954d6f9804d
kai <aaaa666657>
2021-05-31 04:11:06 +0000

    I need healing

---
+++ Control/go.mod
@@ -0,0 +1,15 @@
+module control
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/syhlion/sqlwrapper v0.0.0-20180809095110-10e44a05844b // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	google.golang.org/genproto v0.0.0-20210524171403-669157292da3 // indirect
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
+)
--- Control/server/main.go
+++ Control/server/main.go
@@ -1,26 +1,73 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
+	"strings"
 
+	scannerPB "control/proto/scanner"
+
+	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
+)
 
+const (
+	userName = "root"
+	password = "scanner"
+	host     = "127.0.0.1"
+	port     = "3306"
+	dbName   = "scanner"
 )
 
 type Server struct{}
 
+func (*Server) Register(ctx context.Context, req *scannerPB.ResourceRegister) (*scannerPB.ResourceRegisterResult, error) {
+	fmt.Printf("Got Data %v \n", req)
+
+	port := req.GetPort()
+	service := req.GetServiceType()
+
+	res := &scannerPB.ResourceRegisterResult{
+		Result: true,
+	}
+
+	fmt.Printf("Got Dataus %d %s  \n", port, service)
+
+	return res, nil
+}
+
 func main() {
+	//DB
+	path := strings.Join([]string{userName, ":", password, "@tcp(", host, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	fmt.Println(path)
+
+	DB, _ := sql.Open("mysql", path)
+
+	// 設定 database 最大連接數
+	DB.SetConnMaxLifetime(100)
+
+	//設定上 database 最大閒置連接數
+	DB.SetMaxIdleConns(10)
+
+	// 驗證是否連上 db
+	if err := DB.Ping(); err != nil {
+		fmt.Println("opon database fail:", err)
+		return
+	}
+	fmt.Println("connnect success")
+	//gRPC Server
 	fmt.Println("starting gPRC seerver...")
-	lis, err := net.Listen("tcp", "localhost:50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatal("failed to listen: %v \n", err)
+		//log.Fatal("failed to listen: %v \n", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	scannerPD.RegisterResourceRegisterServiceServer(grpcServer,$Server{})
-	
+	scannerPB.RegisterResourceRegisterServiceServer(grpcServer, &Server{})
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v \n", err)
 	}
